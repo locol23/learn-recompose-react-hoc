b3c1034448370337b4905ce2d6822f5b993a778f
Yoshitaka Terazawa <locol6622@gmail.com>
2018-05-07 20:31:02 +0900

    chore: rename dir name

---
+++ pure/src/components/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { compose, setDisplayName, withStateHandlers } from 'recompose'
+import Child from './Child'
+
+const ButtonText = props => {
+  const handler = () =>
+    props.text === 'hoge' ? props.setText('fuga') : props.setText('hoge')
+  return (
+    <div>
+      <button onClick={handler}>Change text state</button>
+    </div>
+  )
+}
+
+const ButtonShow = props => (
+  <div>
+    <button onClick={props.setShow}>Change show state</button>
+  </div>
+)
+
+const Component = props => (
+  <div>
+    <Child text={props.text} />
+    <ButtonText {...props} />
+    <div>{String(props.show)}</div>
+    <ButtonShow {...props} />
+  </div>
+)
+
+const Enhance = compose(
+  setDisplayName('App'),
+  withStateHandlers(
+    {
+      text: 'hoge',
+      show: false,
+    },
+    {
+      setText: () => t => ({ text: t }),
+      setShow: ({ show }) => () => ({ show: !show }),
+    }
+  )
+)
+
+export default Enhance(Component)
--- pure/src/components/Child.js
+++ pure/src/components/Child.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { compose, setDisplayName, pure } from 'recompose'
 
-const Component = props => <div>{props.text}</div>
+const Component = ({ text }) => (
+  <div>
+    {text}
+    {console.log('render Child Component')}
+  </div>
+)
 
 const Enhance = compose(setDisplayName('Child'), pure)
 
